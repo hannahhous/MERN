459561f10efaf48f72ead623eb353e8f34e824c7
hannahhous <hjh21a@fsu.edu>
2024-02-28 19:11:44 -0500

    idk

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
+import connectDB from './config/db.js';
 import products from "./data/products.js"
+
 
 //const express = require('express');
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
 
