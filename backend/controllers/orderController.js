9964b7d096a3342501243b6d6bd6b1f9a60a3a3c
hannahhous <hjh21a@fsu.edu>
2024-03-26 02:07:47 -0400

    done with section 10

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -70,14 +70,33 @@ const getOrderbyId = asyncHandler(async (req, res) => {
 
 //!!! idk if this is right because im saying :_id
 // @desc    Update order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-    res.send('update order to paid');
+    const order = await Order.findById(req.params.id);
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+
+        }
+        const updatedOrder = await order.save();
+        res.status(200).json(updatedOrder);
+    }
+    else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+
+
 });
 
-// @desc    Update order ot delivered
-// @route   POST /api/orders/:id/deliver
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
     res.send('update order to delivered');
